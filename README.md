066e9077c4aec699cf7be534c254b7e3d0d05194
Alex Godfrey <alexjamesgodfrey@gmail.com>
2020-11-15 20:36:01 -0500

    First Commit (Non-Gui)

---
+++ README.md
@@ -0,0 +1,29 @@
+# Flower Classifier Created By Alex Godfrey 11/15/2020
+
+## Background
+This project was originally created in order to satisfy a project requirement for the Udacity Machine Learning Nanodegree Program, and later expanded to increase accuracy and let users test their own images.
+
+## Installation
+Download flower-classify.py, flower-compatibility.txt, test-images folder, and desired test images from google.
+
+## Neural Network Information
+There are currently three models available to choose from. (4) is a model trained on 1 dense layer neural network with 102 outputs over four epochs, and the same for (6) six epoch and (8) eight epoch networks
+The models have accuracies of 73%, 76%, and 77%, respectively
+
+## Usage
+1. Run flower-classify.py
+2. Enter the path for the image you want to test (e.g. images/wild_pansy.jpg)
+3. Enter which neural network you would like to use (4, 6, or 8) epoch nets are available (e.g. 4)
+4. Enter the number of top predictions you would like to see (max 102) (e.g. 5)
+5. Wait for calculations to complete to see results!
+
+
+
+
+
+
+
+
+[Dataset Used][dataset]
+
+[dataset]: http://www.robots.ox.ac.uk/~vgg/data/flowers/102/
---
+++ flower-classify.py
@@ -0,0 +1,105 @@
+ #imports
+import time
+from time import sleep
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_hub as hub
+import json
+from PIL import Image
+
+
+print()
+print()
+print()
+
+print('Welcome to the Flower Classifer! Please follow the steps to classify your flower.')
+time.sleep(1)
+
+#select image
+print()
+print('1. Enter the path of prediction image')
+image_path = input()
+print()
+
+#select model
+def model_selection():
+    print('2. Which neural network would you like to use? (Enter 4, 6, or 8) The number corresponds to the number of epochs the network was trained on.')
+    model = input()
+    while model != '4' and model != '6' and model != '8':
+        print('Invalid model. Try again')
+        model = input()
+    if model == '4':
+        model = 'neural-networks/four_epoch.h5'
+        return model
+    elif model == '6':
+        model = 'neural-networks/six_epoch.h5'
+        return model
+    elif model == '8':
+        model = 'neural-networks/eight_epoch.h5'
+        return model
+
+model = model_selection()
+print()
+
+#select prediciton count
+print('3. Enter the desired number of top predictions')
+top_k = input()
+top_k = int(top_k)
+print()
+
+print('CALCULATING {} PROBABILITIES FOR {} using {} NEURAL NETWORK'.format(top_k, image_path, model))
+sleep(3)
+
+#load json dictionary
+category_names = 'label_map.json'
+with open(category_names, 'r') as f:
+    class_names = json.load(f)
+
+#load model
+def load_model(model):
+    loaded = tf.keras.models.load_model(model, custom_objects={'KerasLayer':hub.KerasLayer})
+    return loaded
+loaded_model = load_model(model)
+
+#process image function
+def process_image(input_array):
+    tensor = tf.convert_to_tensor(input_array)
+    resized = tf.image.resize(tensor, (224, 224))
+    normalized = tf.math.divide(resized, 255)
+    final = normalized.numpy()
+    return final
+
+#predict function
+def predict(image_path, model, top_k):
+    im = Image.open(image_path)
+    test_image = np.asarray(im)
+    processed_test_image = process_image(test_image)
+    with_1 = np.expand_dims(processed_test_image, axis=0)
+    to_tensor = tf.convert_to_tensor(with_1)
+
+    predictions = loaded_model.predict(to_tensor)[0]
+    labels_unfinished = (-predictions).argsort()[:top_k]
+    labels_complete = [x + 1 for x in labels_unfinished]
+
+    preds = []
+    for i in labels_unfinished:
+        preds.append(predictions[i])
+
+
+    return preds, labels_complete
+
+#output
+probs, classes = predict(image_path, model, top_k)
+classes_with_names = []
+for i in range(len(classes)):
+    classes_with_names.append(class_names[str(classes[i])])
+
+print()
+print('~~~RESULTS~~~')
+for i in range(len(probs)):
+    percentage = round((probs[i] * 100), 2)
+    species = classes_with_names[i]
+    print('{}% percent chance of {}'.format(percentage, species))
+    print()
+    time.sleep(1)
